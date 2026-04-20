2946541a236814699587e97108deb49418d2bea6
arfath07-hub <arfath07v@gmail.com>
2026-04-20 20:04:24 +0530

    Add files via upload

---
+++ while_condition.py
@@ -0,0 +1,25 @@
+#while condition
+i=1
+while i <=5:
+    print(i)
+    i+=1
+    
+
+#sum of two numbers
+sum = 0
+i =1
+while i<=10:
+    sum +=i
+    i+=1
+print(sum)
+
+for i in range(10):
+    if i ==3:
+        break
+    print(i)
+
+
+for i in range(5):
+    if i ==2:
+        continue
+    print(i)
